f47da425c370c36dd19f28d5a6f9059f518eb194
sanicboi <alexandrzezekalo@gmail.com>
2026-08-08 19:55:41 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(async () => {
+  if (!localStorage.getItem('token')) {
+    await router.push('/login')
+  }
+})
+</script>
+
+<template></template>
